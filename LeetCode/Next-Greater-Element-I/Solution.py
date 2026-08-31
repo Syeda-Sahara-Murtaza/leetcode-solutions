2d00db0f811821e7503bb14a1c6e39de687ca12e
syeda sahara murtaza <syedasaharamurtaza01@gmail.com>
2026-08-31 00:02:07 -0700

    Add solution for Next Greater Element I

--- LeetCode/Next-Greater-Element-I/Solution.py
+++ LeetCode/Next-Greater-Element-I/Solution.py
@@ -1,17 +1,37 @@
 1class Solution:
 2    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-3        ans = []
-4        for i in range(len(nums1)):
-5            found = False
-6            for j in range(len(nums2)):
-7                if nums1[i] == nums2[j]:
-8                    for k in range(j + 1, len(nums2)):
-9                        if nums2[k] > nums2[j]:
-10                            ans.append(nums2[k])
-11                            found = True
-12                            break
-13                    if not found:
-14                        ans.append(-1)
-15                    break
-16        return ans
-17        
+3        stack = []
+4        greater = {}
+5        for num in nums2:
+6            while stack and stack[-1] < num:
+7                greater[stack.pop()] = num
+8            stack.append(num)
+9        while stack:
+10            greater[stack.pop()] = -1
+11        return [greater[num] for num in nums1]
+12        
+13 
+14        
+15
+16
+17
+18
+19
+20
+21
+22# class Solution:
+23#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+24        # ans = []
+25        # for i in range(len(nums1)):
+26        #     found = False
+27        #     for j in range(len(nums2)):
+28        #         if nums1[i] == nums2[j]:
+29        #             for k in range(j + 1, len(nums2)):
+30        #                 if nums2[k] > nums2[j]:
+31        #                     ans.append(nums2[k])
+32        #                     found = True
+33        #                     break
+34        #             if not found:
+35        #                 ans.append(-1)
+36        #             break
+37        # return ans
